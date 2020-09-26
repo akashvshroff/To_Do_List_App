72d7e83051824114f4a8488539e112a451f1b2ec
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-09-26 11:53:00 +0530

    Added colours for the project

--- lib/colours.dart
+++ lib/colours.dart
@@ -1,1 +1,30 @@
 import 'package:flutter/material.dart';
+
+Color bgColorPrimary = Colors.black;
+Color textColor = Colors.white;
+Color bgColorSecondary = Colors.grey[850];
+Color redButton = Colors.redAccent;
+Color blueButton = Colors.lightBlueAccent;
+Color greenButton = Colors.greenAccent;
+
+Map categoryColorsMap = {
+  'red': Colors.redAccent[100],
+  'orange': Colors.deepOrangeAccent[100],
+  'yellow': Colors.amberAccent[100],
+  'green': Colors.lightGreenAccent[100],
+  'blue': Colors.cyanAccent[100],
+  'indigo': Colors.indigoAccent[100],
+  'grey': Colors.blueGrey[300],
+  'brown': Colors.brown[400],
+};
+
+List<String> categoryColorsList = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'indigo',
+  'grey',
+  'brown'
+];
