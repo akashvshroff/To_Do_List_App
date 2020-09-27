918105f2fed8d323f259b9a80288a70c45a47d98
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-09-27 15:41:40 +0530

    Built db helper modal

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,7 +1,36 @@
 class Category {
-  int categoryId;
-  String categoryName;
-  String categoryColour;
+  int _categoryId;
+  String _categoryName;
+  String _categoryColour;
 
-  Category(this.categoryName, this.categoryId, this.categoryColour);
+  Category(this._categoryName, this._categoryColour);
+  Category.withId(this._categoryName, this._categoryId, this._categoryColour);
+
+  int get categoryId => _categoryId;
+  String get categoryName => _categoryName;
+  String get categoryColour => _categoryColour;
+
+  set categoryColour(String newColour) {
+    this._categoryColour = newColour;
+  }
+
+  set categoryName(String newName) {
+    this._categoryName = newName;
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (categoryId != null) {
+      map['categoryId'] = _categoryId;
+    }
+    map['categoryName'] = _categoryName;
+    map['categoryColour'] = _categoryColour;
+    return map;
+  }
+
+  Category.fromMapObject(Map<String, dynamic> map) {
+    this._categoryId = map['categoryId'];
+    this._categoryColour = map['categoryColour'];
+    this._categoryName = map['categoryName'];
+  }
 }
