65a0a4e8077362ec33748435de5e821df2c5027a
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-09-27 15:41:58 +0530

    Built db helper model for tasks

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,11 +1,63 @@
 class Task {
-  int id;
-  String taskName;
-  String taskDescription;
-  int taskCategory;
-  int taskPriority;
-  bool isChecked;
+  int _id;
+  String _taskName;
+  String _taskDescription;
+  int _taskCategory;
+  int _taskPriority;
+  bool _isChecked;
 
-  Task(this.taskName, this.taskDescription, this.taskPriority,
-      this.taskCategory, this.isChecked);
+  Task(this._taskName, this._taskDescription, this._taskPriority,
+      this._taskCategory, this._isChecked);
+
+  Task.withId(this._id, this._taskName, this._taskDescription,
+      this._taskPriority, this._taskCategory, this._isChecked);
+
+  int get id => _id;
+  String get taskName => _taskName;
+  String get taskDescription => _taskDescription;
+  int get taskCategory => _taskCategory;
+  int get taskPriority => _taskPriority;
+  bool get isChecked => _isChecked;
+
+  set taskName(String newName) {
+    this._taskName = newName;
+  }
+
+  set taskCategory(int newCategory) {
+    this._taskCategory = newCategory;
+  }
+
+  set taskDescription(String newDescription) {
+    this._taskDescription = newDescription;
+  }
+
+  set taskPriority(int newPriority) {
+    this._taskPriority = newPriority;
+  }
+
+  set isChecked(bool newChecked) {
+    this._isChecked = newChecked;
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = _id;
+    }
+    map['taskName'] = _taskName;
+    map['taskDescription'] = _taskDescription;
+    map['taskPriority'] = _taskPriority;
+    map['taskCategory'] = _taskCategory;
+    map['isChecked'] = (_isChecked) ? 1 : 0;
+    return map;
+  }
+
+  Task.fromMapObject(Map<String, dynamic> map) {
+    this._id = map['id'];
+    this._taskName = map['taskName'];
+    this._taskDescription = map['taskDescription'];
+    this._taskPriority = map['taskPriority'];
+    this._taskCategory = map['taskCategory'];
+    this._isChecked = (map['isChecked'] == 1) ? true : false;
+  }
 }
